401ae29c0b5cfc1d179958718df55723dd6d1321
nabeel yahya <yahyanabeel311@gmail.com>
2022-08-03 20:10:30 +0300

    First commit

---
+++ MtFirst MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MtFirst_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
